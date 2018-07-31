2133850e02ae2c9c437cc131f3bb8664bc7bfaa6
Bartek B. <bartek.bugala@gmail.com>
2018-07-31 18:31:33 +0200

    Merge origin/master
    Conflicts:
    	README.md

---
+++ views/site/oferta.php
@@ -0,0 +1,20 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Oferta - Zabiegi Stomatologiczne';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Oferta Usług Stomatologicznych</h1>
+
+        <p class="lead">Stomatolodzy Gabinetu "Ząbek".</p>
+ </div>
+
+    <div class="body-content">
+
+        
+
+    </div>
+</div>
---
+++ views/site/stomatolodzy.php
@@ -0,0 +1,20 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Stomatolodzy - Gabinetu Stomatologicznego "Ząbek"';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Lekarze Stomatolodzy</h1>
+
+        <p class="lead">Oferta Gabinetu Stomatologicznego "Ząbek".</p>
+ </div>
+
+    <div class="body-content">
+
+        
+
+    </div>
+</div>
