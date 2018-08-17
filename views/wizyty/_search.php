ba1ba149ad9b71ab37d591f3b1c865db92b0e3ae
Bartek B. <bartek.bugala@gmail.com>
2018-08-17 12:00:45 +0200

    Zmiana "szukaj" na "potwierdź" w Formularzu Wizyty

--- views/wizyty/_search.php
+++ views/wizyty/_search.php
@@ -52,7 +52,7 @@ use yii\helpers\ArrayHelper;
     ]);?>
 
     <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Szukaj'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::submitButton(Yii::t('app', 'Potwierdź'), ['class' => 'btn btn-primary']) ?>
         <?= Html::a('Wyczysc formularz', ['create'], ['class' => 'btn btn-default']) ?>
     </div>
 
