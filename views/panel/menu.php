40af95db29eadfd31be9d2dc845304f46a652c21
Abdul <Abdul@Abdul-Komputer>
2018-08-19 21:17:19 +0200

    poprawiono link do głównej strony przychodni

--- views/panel/menu.php
+++ views/panel/menu.php
@@ -1,7 +1,7 @@
 <div class="container">
         <div class="card">    
 <nav class="navbar navbar-expand-lg navbar-primary bg-light">
-  <a class="navbar-brand" href="http://localhost/regastoNB/regasto/web/">ReGaSto</a>
+  <a class="navbar-brand" href="../../web/index.php">ReGaSto</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
