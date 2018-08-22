65ca6b788e6c7f66dd92b9ae7c4d809963c4ea97
Bartek B. <bartek.bugala@gmail.com>
2018-08-22 23:41:10 +0200

    Generator Wolnego :D gotowy

--- web/php/panel/PDO_Wolne.php
+++ web/php/panel/PDO_Wolne.php
@@ -7,15 +7,14 @@ require_once '../../../config/param_db.php';
 // Skrypt wylicza daty z godzinami co 30 minut = 1800 sekund + usuwa z tablicy godziny nierobocze
 
 // DEKLARACJA Zmiennych
-if (isset($_SESSION['zalogowany'], $_POST['dodaj']) && ($_SESSION['role'] === '30' || $_SESSION['role'] === '20' || $_SESSION['role'] === '15'))
-{
+if (isset($_SESSION['zalogowany'], $_POST['dodaj']) && ($_SESSION['role'] === '30' || $_SESSION['role'] === '20' || $_SESSION['role'] === '15')) {
     $date;
     $time;
     $data_poczatkowa = $_POST['data_poczatkowa']; // tutaj można ustawić zmienną (z datepickera)
     $data_koncowa = $_POST['data_koncowa'];  // tutaj można ustawić zmienną (z datepickera)
-    /* Dane bazy danych*/
+    /* Dane bazy danych */
 
- require_once 'PDO_bd.php';
+    require_once 'PDO_bd.php';
 
 
 
@@ -32,37 +31,49 @@ if (isset($_SESSION['zalogowany'], $_POST['dodaj']) && ($_SESSION['role'] === '3
 
         if (!in_array(date('w', $t), $dni_tygodnia_wylaczone) && !in_array(date('H', $t), $godziny_wylaczone)) {
             //$tablica_dat[$i] = date('Y-m-d H:i:s', $t);
-            $datetime =  date('Y-m-d H:i:s', $t);
+            $datetime = date('Y-m-d H:i:s', $t);
             $date_arr = explode(" ", $datetime);
-            $date= $date_arr[0];
-            $time= $date_arr[1];
+            $date = $date_arr[0];
+            $time = $date_arr[1];
             $query = "INSERT INTO wizyty (id_pacjenta, data, godzina) VALUES ('1', :date, :time);";
             $statement = $connect->prepare($query);
 
-             try {
-                 $statement->execute(
-                array(
-                    ':date' => $date,
-                    ':time' => $time,
+            try{
+                try {
+                $statement->execute(
+                        array(
+                            ':date' => $date,
+                            ':time' => $time,
+                        )
+                );
+            } catch (Exception $e) {
+
+                $query2 = "UPDATE wizyty SET id_pacjenta='1' WHERE data = :date AND godzina = :time";
+                $statement2 = $connect->prepare($query2);
+                $statement2->execute(
+                        array(
+                            ':date' => $date,
+                            ':time' => $time,
                 )
-
-        );} catch (Exception $e) {
-
-    echo '<br><br><b>BŁĄD! Prawdopodobnie zdublowane wpisy sprawdź dokładnie wykasuj lub podaj prawidłowe ramy czasowe:</b><br><br>Komunikat błędu: ',  $e->getMessage(), "\n";
-     exit;
-}
-    }
-    }
+                        );
             }
-else
+        }catch (Exception $e) {
+            echo 'ZA DUŻO WOLNEGO NA RAZ - MNIEJSZE PORCJE';
+            exit;
+            
+        }
+        }
+    }
+} else
 {}
     if (isset($_SESSION['role']) && $_SESSION['role'] === '30' )
 {
     ?>
 <H1>Generator DNI Wolnych od pracy do bazy</H1>
 <p>Tutaj Można dodawać dni i godziny wolne poza weekendami w godzinach pracy.<br>
-W celu wybrania jednego dnia trzeba po prostu ustawić godziny od 07:00 do 22:00</p>
-    <form action="PDO_Terminy.php" method="POST">
+    W celu wybrania jednego dnia trzeba po prostu ustawić godziny od 07:00 do 22:00<br>
+         UWAGA NA OGRANICZENIE CZASOWE - URLOPY I DNI WOLNE DODAWAĆ MNIEJSZYMI ODCINKAMI</p>
+    <form action="PDO_Wolne.php" method="POST">
         <div class="form-group">
             <div class="row">
                 <div class="col">
