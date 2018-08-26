e8e0c3148cdfa050c75d165ff1869f520e8fc5bd
Bartek B. <bartek.bugala@gmail.com>
2018-08-26 16:54:45 +0200

    RODO

--- views/site/rodo.php
+++ views/site/rodo.php
@@ -9,8 +9,7 @@ $this->title = 'RODO - zasady';
     <div class="jumbotron">
         <h1>Zasady i reguły związane z RODO</h1>
 
-        <p class="lead">W związku z RODO jeśli nie chcesz podawać danych osobowych przyjdź w worku na głowie z otworem na jamę ustną w celu wykonania anonimowego zabiegu. Jeśli zgadzasz się na przetwarzanie danych osobowych, możesz być pewny, że wykorzystamy je po to, abyś następnym razem nie ryzykował wyrywania protez dentystycznych dzięki zachowaniu historii leczenia. Jeśli życzysz sobie narazić się na tego typu problemy możemy przydzielić ci jedynie numer pacjenta i po tym numerze będziesz identyfikowany, jeśli wstydzisz się występować w roli pacjenta u dentysty możesz udawać poborcę podatkowego, albo typowego przedstawiciela brytyjskiej klasy robotniczej pochodzącego z Westminster w hrabstwie Sutterland
-Przy formularzu rejestracji trzeba koniecznie dodać checkbox akceptacji wszystkich najgłupszych regulaminów.
+        <p class="lead">W związku z RODO jeśli nie chcesz podawać danych osobowych przyjdź w worku na głowie z otworem na jamę ustną w celu wykonania anonimowego zabiegu. Jeśli zgadzasz się na przetwarzanie danych osobowych, możesz być pewny, że wykorzystamy je po to, abyś następnym razem nie ryzykował wyrywania protez dentystycznych dzięki zachowaniu historii leczenia. Jeśli życzysz sobie narazić się na tego typu problemy możemy przydzielić ci jedynie numer pacjenta i po tym numerze będziesz identyfikowany, jeśli wstydzisz się występować w roli pacjenta u dentysty możesz udawać poborcę podatkowego, albo typowego przedstawiciela brytyjskiej klasy robotniczej pochodzącego z Westminster w hrabstwie Sutterland.
 W związku z RODO zmieniliśmy szyld i napisy na gabinecie na "Ministerstwo Głupich Kroków" prosimy jednak o nie zachowywanie dyskrecji, gdyż chcemy, aby
 poczta pantoflowa zapewniła nam pacjentów z kontrabandy.
 Możesz być pewny, że nikt nie dowie się, że boli cię piątka, a w szóstce masz stan zapalny</p>
