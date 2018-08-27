41fc60dcd822e1bd763db18d075b911324646fe3
Bartek B. <bartek.bugala@gmail.com>
2018-08-27 23:27:17 +0200

    CSS #3

--- views/site/stomatolodzy.php
+++ views/site/stomatolodzy.php
@@ -1,6 +1,7 @@
 <?php
 
 /* @var $this yii\web\View */
+require_once '../config/param_stomatolog.php';
 
 $this->title = 'Stomatolodzy - Gabinetu Stomatologicznego "Ząbek"';
 ?>
@@ -13,8 +14,13 @@ $this->title = 'Stomatolodzy - Gabinetu Stomatologicznego "Ząbek"';
  </div>
 
     <div class="body-content">
-
-        
-
+        <div class="row">
+        <div class="col-md-6"><img  src="http://pp43704.wsbpoz.solidhost.pl/ReGaSto/stom1.jpg" title="Stomatolog <?php echo $stom1;?>" alt="<?php echo $stom1;?>"></div>
+        <div class="col-md-6"><img src="http://pp43704.wsbpoz.solidhost.pl/ReGaSto/stom2.jpg" title="Stomatolog <?php echo $stom2;?>" alt="<?php echo $stom2;?>"/></div>
+        </div>
+        <div class="row">
+            <div class="col-md-6"><h4>Lek. Stom. <?php echo $stom1;?></h4></div>
+            <div class="col-md-6"><h4>Lek. Stom. <?php echo $stom2;?></h4></div>
+        </div>
     </div>
 </div>
