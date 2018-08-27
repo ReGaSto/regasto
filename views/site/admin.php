f643205d243df74e0285d0e49bd8d67f1db1ef92
Abdul <Abdul@Abdul-Komputer>
2018-08-28 00:52:51 +0200

    dodano odnośniki do zarządzania tabelami przez Yii

--- views/site/admin.php
+++ views/site/admin.php
@@ -1,5 +1,4 @@
 <?php
-
 /* @var $this yii\web\View */
 
 use yii\helpers\Html;
@@ -10,82 +9,36 @@ $this->title = 'Panel admina Yii2';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-index">
-
+    <!--M.Kurant ustawiono guziki do zarządzania tabelami-->
     <div class="jumbotron">
         <h1>Panel admina Yii2</h1>
-    <?php
-if (Yii::$app->user->identity->role === 30){
-    ?>
-        <p class="lead">Z sukcesem otworzyłeś tą stronę!</p>
+        <?php
+        if (Yii::$app->user->identity->role === 30) {
+            ?>
+            <p class="lead">Z sukcesem otworzyłeś tą stronę!</p>
 
-        <p><a class="btn btn-lg btn-success" href="<?php echo Url::toRoute('wizyty-admin/index'); ?>">Zacznij zarządzać tabelą rezerwacji wizyt &raquo;</a></p>
-    </div>
+            <p><a class="btn btn-lg btn-success" href="<?php echo Url::toRoute('wizyty-admin/index'); ?>">Zacznij zarządzać tabelą rezerwacji wizyt &raquo;</a></p>
 
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Tutaj można dodać kolejne przyciski</h2>
-
-                <p>A nawet zrobić krótki opis.</p>
-
-                <p><a class="btn btn-default" href="<?php echo Url::to('@web/php/panel/logpanel.php') ?>">Panel admina PHP &raquo;</a></p>
-            </div>
-            <div class="row">
-            <div class="col-lg-4">
-                <h2>Tutaj można dodać kolejne przyciski</h2>
-
-                <p>A nawet zrobić krótki opis.</p>
-
-                <p><a class="btn btn-default" href="<?php echo Url::to('@web/php/panel/logpanel.php') ?>">Panel admina PHP &raquo;</a></p>
-            </div>
+            <div class="group">
                 <div class="row">
-            <div class="col-lg-4">
-                <h2>Tutaj można dodać kolejne przyciski</h2>
-
-                <p>A nawet zrobić krótki opis.</p>
-
-                <p><a class="btn btn-default" href="<?php echo Url::to('@web/php/panel/logpanel.php') ?>">Panel admina PHP &raquo;</a></p>
-            </div>
-
-        </div>
-    </div>
-           <!-- Kolejny wiersz -->
-    </div>
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Tutaj można dodać kolejne przyciski</h2>
-
-                <p>A nawet zrobić krótki opis.</p>
-
-                <p><a class="btn btn-default" href="<?php echo Url::to('@web/php/panel/logpanel.php') ?>">Panel admina PHP &raquo;</a></p>
-            </div>
-            <div class="row">
-            <div class="col-lg-4">
-                <h2>Tutaj można dodać kolejne przyciski</h2>
-
-                <p>A nawet zrobić krótki opis.</p>
-
-                <p><a class="btn btn-default" href="<?php echo Url::to('@web/php/panel/logpanel.php') ?>">Panel admina PHP &raquo;</a></p>
-            </div>
+                    <div class="col">
+                        <p><a class="btn btn-lg btn-success" href="<?php echo Url::to('stomatolodzy-admin/index') ?>">Zarządzanie tabelą stomatologów &raquo;</a></p>
+                    </div></div>
                 <div class="row">
-            <div class="col-lg-4">
-                <h2>Tutaj można dodać kolejne przyciski</h2>
+                    <div class="col">
+                        <p><a class="btn btn-lg btn-success" href="<?php echo Url::to('new-user-admin/index') ?>">Zarządzanie tabelą użytkowników &raquo;</a></p>
+                    </div></div>
+                <div class="row">    
+                    <div class="col">
+                        <h4>Więcej ciekawych opcji (komunikacja, generatory, recepta)</h4>
+                        <p><a class="btn btn-lg btn-success" href="<?php echo Url::to('@web/php/panel/logpanel.php') ?>">Panel admina PHP &raquo;</a></p>
+                    </div></div>
 
-                <p>A nawet zrobić krótki opis.</p>
+            </div></div>
 
-                <p><a class="btn btn-default" href="<?php echo Url::to('@web/php/panel/logpanel.php') ?>">Panel admina PHP &raquo;</a></p>
-            </div>
-
-        </div>
-    </div>
-    </div>
-    </div>
-    <?php
+        <?php
+    } else {
+        echo 'Nie masz uprawnień do przeglądania tej strony.';
     }
-else
-{
-    echo 'Nie masz uprawnień do przeglądania tej strony.';
-}
-?>
+    ?>
 </div>
