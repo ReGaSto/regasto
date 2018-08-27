c4ae1ed2f136b819dd56c91fb507ad7c435c2f50
Abdul <Abdul@Abdul-Komputer>
2018-08-27 22:57:09 +0200

    ciekawy commit

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,6 +21,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/regasto.css'
     ];
     public $js = [
         'js/main.js',
