531fbb85a2ae22a96f05cd6ddc43f07c4c0d1dbe
Bartek B. <bartek.bugala@gmail.com>
2018-08-27 05:41:27 +0200

    PDO_terminy

--- config/param_stomatolog.php
+++ config/param_stomatolog.php
@@ -1,7 +1,39 @@
 <?php
 $stom1 = 'Jan Borowiec';
 $stom2 = 'Konrad Rwaniec';
-// Dni pracy stomatologów (1- poniedziałek, 2- wtorek, 3-środa, 4, czwartek, 5-piątek)
-$dni_pracy_stom1 = array('1','3','5');
-$dni_pracy_stom2 = array('2','4');    
+
+    $dni_pracy_stom1 = array('1','3','5');
+    $dni_pracy_stom2 = array('2','4');   
+
+//$query_stom_imie1 = "SELECT `imie_nazwisko` FROM `stomatolodzy` WHERE `id_stomatologa` = 1;";
+//$statement_stom_imie1 = $connect->prepare($query_stom_imie1);
+//$statement_stom_imie1->execute();
+//$row_stom_imie1 = $statement_stom_imie1->fetch();
+//
+//$stom1 = $row_stom_imie1;
+//$stom1 = implode(" ", $row_stom_imie1);
+//
+//
+//
+//$query_stom_imie2 = "SELECT `imie_nazwisko` FROM `stomatolodzy` WHERE `id_stomatologa` = 2;";
+//$statement_stom_imie2 = $connect->prepare($query_stom_imie2);
+//$statement_stom_imie2->execute();
+//$row_stom_imie2 = $statement_stom_imie2->fetch();
+//
+//$stom2 = implode(" ", $row_stom_imie2);
+//
+//
+//
+//$query_stom1 = "SELECT `dni_pracy` FROM `stomatolodzy` WHERE `imie_nazwisko` = '$stom1'";
+//$statement_stom1 = $connect->prepare($query_stom1);
+//$statement_stom1->execute();
+//$row_stom1 = $statement_stom1->fetch();
+//$dni_pracy_stom1[] = explode(" ", $row_stom1);
+//
+//$query_stom2 = "SELECT `dni_pracy` FROM `stomatolodzy` WHERE `imie_nazwisko` = '$stom2'";
+//$statement_stom2 = $connect->prepare($query_stom2);
+//$statement_stom2->execute();
+//$row_stom2 = $statement_stom2->fetch();
+//$dni_pracy_stom2[] = explode(" ", $row_stom2);
+
 ?>
